50fa95d2cc52016bfccdbe283f725d6d98a953b2
RomChung <thelns@vip.qq.com>
2019-10-28 19:33:44 +0800

    no message

--- src/pages/practise/evaluate/index.jsx
+++ src/pages/practise/evaluate/index.jsx
@@ -3,12 +3,53 @@ import { Row, Col, Table, Button, Modal, Form, Input, DatePicker, Rate } from 'a
 import './course.scss';
 const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
 
-class EvaluateView extends React.PureComponent {
-  static defaultProps = {
+const { useState } = React;
+export default function() {
+  const [visible, setVisible] = useState(false);
+  const [name, setName] = useState('');
+  const [author, setAuthor] = useState('');
+  const [desc, setDesc] = useState('');
+  const handleClick = () => {
+    setVisible(true)
+  }
+  const data = [
+    {
+      key: '1',
+      name: 'C语言',
+      author: '测试老师',
+      time: '2019/10/10 08:20:13',
+      star: <Rate  defaultValue={3} />,
+      action: <div><Button onClick={handleClick} type="primary">评价</Button></div>
+    },
+    {
+      key: '2',
+      name: '软件测试',
+      author: '测试老师',
+      time: '2019/10/10 08:22:30',
+      star: <Rate disabled defaultValue={0} />,
+      action: <div><Button onClick={handleClick} type="primary">评价</Button></div>
 
-  };
+    },
+    {
+      key: '3',
+      name: '软件工程',
+      author: '测试老师',
+      time: '2019/10/10 08:33:42',
+      star: <Rate disabled defaultValue={4} />,
+      action: <div><Button onClick={handleClick} type="primary">评价</Button></div>
 
-  columns = [
+    },
+    {
+      key: '4',
+      name: '测试',
+      author: '测试老师',
+      time: '2019/10/10 08:42:15',
+      star: <Rate disabled defaultValue={5} />,
+      action: <div><Button onClick={handleClick} type="primary">评价</Button></div>
+
+    },
+  ];
+  const columns = [
     {
       title: '课程名称',
       dataIndex: 'name',
@@ -31,108 +72,46 @@ class EvaluateView extends React.PureComponent {
       dataIndex: 'action',
     }
   ];
-  handleClick = () => {
-    this.setState({
-      visible: true,
-    })
+  const handleOk = () => {
+    setVisible(false)
   }
-  data = [
-    {
-      key: '1',
-      name: 'C语言',
-      author: '测试老师',
-      time: '2019/10/10 08:20:13',
-      star: <Rate  defaultValue={3} />,
-      action: <div><Button onClick={this.handleClick} type="primary">评价</Button></div>
-    },
-    {
-      key: '2',
-      name: '软件测试',
-      author: '测试老师',
-      time: '2019/10/10 08:22:30',
-      star: <Rate disabled defaultValue={0} />,
-      action: <div><Button onClick={this.handleClick} type="primary">评价</Button></div>
-
-    },
-    {
-      key: '3',
-      name: '软件工程',
-      author: '测试老师',
-      time: '2019/10/10 08:33:42',
-      star: <Rate disabled defaultValue={4} />,
-      action: <div><Button onClick={this.handleClick} type="primary">评价</Button></div>
-
-    },
-    {
-      key: '4',
-      name: '测试',
-      author: '测试老师',
-      time: '2019/10/10 08:42:15',
-      star: <Rate disabled defaultValue={5} />,
-      action: <div><Button onClick={this.handleClick} type="primary">评价</Button></div>
-
-    },
-  ];
-  state = {
-    visible: false,
-    name: '',
-    author: '',
-    desc: '',
-
+  const handleInputChange = (keyName) => ({ target }) => {
+    if(keyName === 'Desc') {
+      setDesc(target.value);
+    }
   }
-
-  handleOk = () => {
-    this.setState({
-      visible: false,
-    });
-  }
-  handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
-  handleInputChange = keyName => ({ target }) => {
-    this.setState({
-      [keyName]: target.value,
-    })
-  }
-  render(){
-    return (
-      <div className="course-view">
-        <div className="course-action-bar">
-          <div>
-          </div>
-          <div>
-          </div>
+  return (
+    <div className="course-view">
+      <div className="course-action-bar">
+        <div>
         </div>
-        <Row>
-          <Table
-            columns={this.columns}
-            dataSource={this.data}
-          />
-        </Row>
-        <Modal
-          title="评价"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          okText="确认"
-          cancelText="取消"
-          onCancel={this.handleCancel}
-          width={400}
-        >
-          <Form.Item>
-            <span style={{ color: 'black'}}>评价信息:</span>
-            <Input.TextArea onChange={this.handleInputChange('name')} value={this.state.desc}/>
-          </Form.Item>
-          <Form.Item>
-            <span style={{ color: 'black'}}>评价星级:</span>
-              <Rate />
-          </Form.Item>
-        </Modal>
+        <div>
+        </div>
       </div>
-    )
-  }
+      <Row>
+        <Table
+          columns={columns}
+          dataSource={data}
+        />
+      </Row>
+      <Modal
+        title="评价"
+        visible={visible}
+        onOk={handleOk}
+        okText="确认"
+        cancelText="取消"
+        onCancel={() => {setVisible(false)}}
+        width={400}
+      >
+        <Form.Item>
+          <span style={{ color: 'black'}}>评价信息:</span>
+          <Input.TextArea onChange={handleInputChange('Desc')} value={desc}/>
+        </Form.Item>
+        <Form.Item>
+          <span style={{ color: 'black'}}>评价星级:</span>
+          <Rate />
+        </Form.Item>
+      </Modal>
+    </div>
+  )
 }
-
-export default EvaluateView;
